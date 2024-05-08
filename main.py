f24c193adbdd764090f166e2696005b03c5fc46e
amax33 <aminreza3880@gmail.com>
2024-05-08 23:31:31 +0330

    base

---
+++ main.py
@@ -0,0 +1,25 @@
+class Subject:
+    def __init__(self, name, general, time):
+        self.name = name
+        self.generality = general
+        self.time = time
+
+
+class Day:
+    def __init__(self, name, certain, hour):
+        self.name = name
+        self.certainty = certain
+        self.hour = hour
+
+
+class Teacher:
+    def __init__(self, degree, expr, days):
+        self.degree = degree
+        self.experience = expr
+        self.days = days
+
+
+class Class:
+    def __init__(self, grade, days):
+        self.grade = grade
+        self.days = days
